59c42196c2dad66f3e50a1bf7a90986cfaf29362
Owen Susanto <owen@susanto.me>
2023-10-27 21:30:29 +0700

    filltering

--- Challenge/Challenge_4/public/scripts/car_form.js
+++ Challenge/Challenge_4/public/scripts/car_form.js
@@ -8,25 +8,37 @@ function getValue(selectObject) {
 async function filter() {
     let carContainerElement = document.getElementById("cars-container");
     carContainerElement.innerHTML = '';
-
+    // 
     let type = getValue(document.getElementById("input-type"));
     let date = getValue(document.getElementById("input-date"));
     let dateTime = new Date(date);
+    //    
     let time = getValue(document.getElementById("input-time"));
     let amount = getValue(document.getElementById("input-amount"));
+    let isEmpty = ((date == '') && (amount == ''))
+    let cars = await CarHelper.listCars(() => {
+        return true;
+    })
+    if (date != '') {
+        cars = cars.filter((car) => {
+            let carDate = new Date(car.availableAt);
+            console.log(carDate, dateTime);
 
-    console.log(type, date, time, amount);
+            let isDateTime = carDate > dateTime;
+            return isDateTime;
+        })
+    }
+    if (amount != '') {
+        cars = cars.filter((car) => {
+            let isAmount = parseInt(car.capacity) >= parseInt(amount);
+            return isAmount;
+        })
+    }
 
-    let cars = await CarHelper.listCars((car) => {
-        let carDate = new Date(car.availableAt);
-        let isDateTime = carDate < dateTime;
-        let isType = true;
-        let isAmount = parseInt(car.capacity) < parseInt(amount);
-        let result = (isDateTime || isAmount || isType) && car.available;
-        return result;
+    cars = cars.filter(car => {
+        return car.available
     })
 
-    console.log(cars);
 
     cars.forEach((car) => {
         const node = document.createElement("div");
@@ -34,7 +46,7 @@ async function filter() {
         <div class="col p-2 mb-5">
             <img src="${car.image}" class="w-auto img-thumbnail object-fit-cover center" style="height:200px;" alt="${car.manufacture}">
             <div>
-                <p class="fs-6 fw-medium">Nama/Tipe Mobil</p>
+                <p class="fs-6 fw-medium">${car.model}/${car.type}</p>
             </div>
             <div class="fs-4 fw-bold">
                 ${car.rentPerDay} / hari
