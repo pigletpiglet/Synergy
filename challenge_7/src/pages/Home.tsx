83d8cce020d4fb50a827abe7f4bc36ea28c5a591
Owen Susanto <owen@susanto.me>
2023-12-08 15:21:44 +0700

    test

--- challenge_7/src/pages/Home.tsx
+++ challenge_7/src/pages/Home.tsx
@@ -1,4 +1,7 @@
+import { Menu } from '@headlessui/react'
 import Header from '../components/Header';
+import { ChevronDownIcon } from '@heroicons/react/24/outline';
+import { useDispatch } from 'react-redux';
 
 interface UserEntity {
   id: number;
@@ -7,17 +10,118 @@ interface UserEntity {
   profile_picture_url: string;
 }
 
-interface TweetEntity {
-  id: number;
-  content: string;
-  user: UserEntity;
-}
 
 
 export default function Home() {
 
+  const tweets_api_base_url = 'http://localhost:8082';
+
+
 
   return (
-    Header()
+    <>
+      {Header()}
+      <div style={{ height: 120 }}></div>
+      <section id="main-section" className="container pl-0">
+        <div className="px-4 py-5 position-relative">
+          <div className='grid grid-cols-2 align-middle'>
+            <div className="col-span-1 p-5">
+              <img className="" src="https://i.ibb.co/zPKWNft/img-car.png" alt="PNG Mobil"></img>
+            </div>
+            <div className="col-span-1 align-middle m-auto">
+              <div className="">
+                <div className=" py-4">
+                  <h1 className="font-bold text-xl">Sewa & Rental Mobil Terbaik di kawasan (Lokasimu)</h1>
+                </div>
+                <p className=''>
+                  Selamat datang di Binar Car Rental. Kami menyediakan mobil kualitas terbaik dengan harga
+                  terjangkau. Selalu siap melayani kebutuhanmu untuk sewa mobil selama 24 jam.
+                </p>
+
+
+              </div>
+
+            </div>
+          </div>
+          <div style={{ bottom: "5px", paddingBottom: "20px" }}
+            className=" container grid grid-cols-12 rounded-lg p-6 shadow-xl border-indigo-200">
+            <div className='col-span-3'>
+              <Menu>
+                <Menu.Button className="inline-flex w-50% justify-center rounded-md px-4 py-2 text-sm font-medium text-black hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75">Tipe Driver <ChevronDownIcon
+                  className="-mr-1 ml-2 h-5 w-5 text-black hover:text-gray-800"
+                  aria-hidden="true"
+                />
+                </Menu.Button>
+                <Menu.Items className="mt-2 w-56 divide-y origin-top-right flex-auto rounded-md shadow-lg ring-1 ring-white focus:outline-none">
+                  <Menu.Item >
+                    {({ active }) => (
+                      <button
+                        onClick={() => { active }}
+                      >
+                        Dengan Driver
+                      </button>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item >
+                    {({ active }) => (
+                      <button
+                        onClick={() => { active }}
+                      >
+                        Tanpa Driver
+                      </button>
+                    )}
+                  </Menu.Item>
+                </Menu.Items>
+              </Menu>
+            </div>
+            <div className="col-span-3">
+              <p>
+                Tanggal
+              </p>
+              <input className="form-control" type="date" name="date" id="input-date">
+              </input>
+
+            </div>
+            <div className="col-span-3">
+              <p>
+                Waktu Jemput/Ambil
+              </p>
+              <input className="form-control" type="time" name="time" id="input-time">
+              </input>
+
+            </div>
+            <div className="col-span-2">
+              <p>
+                Jumlah (Optional)
+              </p>
+              <input className="" id="input-amount">
+              </input>
+            </div>
+            <div className="col-span-1">
+              <button onClick={() => { }} type="button" className="text-white py-2 px-5"
+                style={{ backgroundColor: "#5CB85F" }}>Cari</button>
+            </div>
+          </div>
+        </div>
+
+      </section >
+      <section>
+        <div className='mt-[10px]'>
+          {!tweets.length && <div>Data kosong</div>}
+
+          {tweets &&
+            tweets.map((tweet: TweetEntity) => (
+              <div key={tweet.id} className='mt-3'>
+                <h3>{tweet.content}</h3>
+                <p>
+                  Dibuat oleh <strong>{tweet.user.name}</strong>
+                </p>
+              </div>
+            ))}
+        </div>
+      </section>
+
+
+    </>
   );
 }
