d8d11c9221d00d56cef279783b74d2c01e3666d2
Owen Susanto <owen@susanto.me>
2023-12-09 00:36:51 +0700

    fix small

--- challenge_7/src/App.tsx
+++ challenge_7/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Dashboard />} />
         <Route path='/login' element={<Login />} />
         <Route path='/dashboard' element={<Dashboard />} />
         <Route path='/list' element={<ListCar />} />
